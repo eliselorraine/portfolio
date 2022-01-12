1340b0fec8ce2fcabd7e3a7e3e16ecef8b448270
Elise Lawrence <eliselorraine@Los-Mors-iMac.home>
2022-01-12 12:08:33 +0100

    contact form

--- src/components/about.js
+++ src/components/about.js
@@ -1,11 +1,14 @@
+import '../stylesheets/about.scss'
+
 const About = () => {
     return (
-        <div>
-            My coding journey started in February 2021, when I started playing with HTML, CSS, and Javascript through courses on
+        <div className="about_wrapper">
+            <p className="about_paragraph">My coding journey started in February 2021, when I started playing with HTML, CSS, and Javascript through courses on
             freeCodeCamp.org. I was quickly hooked, and after many hours of my own practice I found myself
-            invited to participate in the intensive &lt;/salt&gt; bootcamp in Stockholm, Sweden. There, I was trained
-            professionally as a full-stack Javascript developer. I am particularly intrigued by creative frontend technologies and
-            like to focus on creating clean, well-structured solutions.
+            invited to participate in the intensive <a className="about_salt" href="https://salt.dev/">&lt;/salt&gt;</a> bootcamp in Stockholm, Sweden.
+            There, I was trained professionally as a full-stack Javascript developer.</p>
+            
+            <p className="about_paragraph">In my code, I am particularly intrigued by creative frontend technologies and love to focus on creating clean, well-structured solutions.</p>
         </div>
     )
 }
--- src/components/contact.js
+++ src/components/contact.js
@@ -1,13 +1,62 @@
 import '../stylesheets/contact.scss'
+import { useState } from 'react';
+import { send } from 'emailjs-com';
 
 const Contact = () => {
+    const from_name = document.getElementById('from_name');
+    const user_email = document.getElementById('user_email');
+    const subject = document.getElementById('subject');
+    const message = document.getElementById('message');
+    
+    const [toSend, setToSend] = useState({
+        from_name: '',
+        user_email: '',
+        subject: '',
+        message: '',
+    })
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        send(
+            'SERVICE ID',
+            'TEMPLATE ID',
+            toSend,
+            'USER ID',
+        ).then((response) => {
+            console.log('Success!', response.status, response.text)
+            from_name.value = '';
+            user_email.value = '';
+            subject.value = '';
+            message.value = '';
+        }).then(() => {
+            setToSend({
+                from_name: '',
+                user_email: '',
+                subject: '',
+                message: '',
+            })
+        }).catch((err) => {
+            console.log('Failed :(', err)
+        })
+    }
+
+
+    const handleChange = (e) => {
+        setToSend({ ...toSend, [e.target.name]: e.target.value });
+    }
+
     return (
         <div className="contact_container">
-            <form className="contact_form">
-                <label htmlFor="subject">Reason for your inquiry:</label>
-                <input id="subject" placeholder="Subject"></input>
-                <label htmlFor="inquiry">Inquiry</label>
-                <input id="inquiry" placeholder="Write your inquiry here..."></input>
+            <form onSubmit={handleSubmit} className="contact_form">
+                <label className="contact_label" htmlFor="from_name">Name:</label>
+                <input name="from_name" className="contact_input" id="from_name" placeholder="Your Name" onChange={handleChange}></input>
+                <label className="contact_label" htmlFor="email">Email:</label>
+                <input name="user_email" className="contact_input" id="user_email" placeholder="Email"></input>
+                <label className="contact_label" htmlFor="subject">Reason for your inquiry:</label>
+                <input name="subject" className="contact_input" id="subject" placeholder="Subject"></input>
+                <label className="contact_label" htmlFor="inquiry">What would you like to be in touch about?</label>
+                <textarea name="message" className="contact_input contact_input--inquiry" id="message" placeholder="Write your inquiry here..." onChange={handleChange}></textarea>
+                <div className="contact_button-container"><button className="contact_button--submit" type="submit" onClick={handleSubmit}>Submit</button></div>
             </form>
         </div>
     )
