dc3ec6c611f9c28754d9a7f2d1a5eeed8b1f88e0
Elise Lawrence <eliselorraine@Los-Mors-iMac.home>
2022-01-19 12:04:02 +0100

    projects

--- src/components/myWork.js
+++ src/components/myWork.js
@@ -7,19 +7,27 @@ const MyWork = () => {
             <div className="projects">
                 <div className="tile-container">
                     <Tile source="todo.gif"
+                        alt="to do list demo"
+                        className="todo-tile"
                         title="To Do List"
                         description="A simple to do app built with React. It gives the user the ability to add a new task, edit the task, mark the task as done, and remove the task from their list. The app saves user's list on refresh."
                         tech="Tech stack: React, Redux, CSS, Jest"
-                        link="https://github.com/eliselorraine/react-todo" />
+                        link="https://github.com/eliselorraine/react-todo"
+                        github="View the code (Github)" />
                     <Tile source="podcast.gif"
                         title="Hook: the Podcast App"
                         description="A fullstack Javascript application for the aspiring podcast listener. Using the Listen Notes API, the user can search for podcast episodes by keyword and save episodes to their list for later listening. Additionally, users can click on the episode thumbnails to view details about the podcast, plus paginate through earlier episodes as well as search results."
                         tech="Tech stack: React, CSS, NodeJS, ExpressJS"
-                        link="https://github.com/eliselorraine/react-todo" />
-                    <Tile source="proCoRecording.gif"
+                        link="https://github.com/eliselorraine/react-todo" 
+                        github="View the code (Github)" />
+                    <Tile
+                        source="proCoScreenshot.png"
+                        alt="proco photo"
                         title="ProCo"
                         description="A fullstack Javascript application for consulting firms and their employees. As a consultant you can register your availability and view your schedule, which is handled by the consulting firm. Consulting firms can use ProCo to assign consultants to companies, view their availability, and schedule them to work. They can also add companies and consultants to their roster. Authentication is required for use of ProCo, which is done through JSON web tokens."
-                        tech="Tech stack: React, Redux, ElephantSQL, NodeJS, ExpressJS, JWT, CSS" />
+                        tech="Tech stack: React, Redux, ElephantSQL, NodeJS, ExpressJS, JWT, CSS"
+                        link="https://youtu.be/A1jlWxb8cx4"
+                        github="View a demo (YouTube)" />
                 </div>
             </div>
         </section>
--- src/components/tile.js
+++ src/components/tile.js
@@ -4,10 +4,11 @@ const Tile = (props) => {
     return (
         <div className="tile" alt="project">
             <div className="tile_description">
+                <img className="tile_demo-gif" src={props.source} alt={props.alt}></img>
                 <h3 className="tile_title">{props.title}</h3>
                 <p className="tile_description--long" >{props.description}</p>
                 <p>{props.tech}</p>
-                <a href={props.link}>View the code (Github)</a>
+                <a href={props.link}>{props.github}</a>
             </div>
         </div>
     )
