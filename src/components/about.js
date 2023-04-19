7347ad268de539c02b6cf4149a316fec2a92f186
Elise Lawrence <eliselorraine@icloud.com>
2023-04-19 20:07:36 +0200

    update about section

--- src/components/about.js
+++ src/components/about.js
@@ -10,16 +10,16 @@ const About = () => {
           </p>
           <p className="about_paragraph">
             I am a full stack web developer who transitioned from a background
-            in classical music. My coding journey began with HTML, CSS, and
-            Javascript through courses on freeCodeCamp.org. After many hours
-            of my own practice and my first certification in Responsive Web Design, I was selected as one of 38 students out of
+            in classical music. Experienced with a variety of tools and technologies, 
+            ranging from important infrastructure tools to modern frontend frameworks.
+            Most importantly I enjoy learning.
+
+            In 2021 I was selected as one of 38 students out of
             1000+ applications to participate in the intensive{" "}
             <a className="about_salt" href="https://salt.dev/">
               &lt;/salt&gt;
             </a>{" "}
-            bootcamp in Stockholm, Sweden. Since graduation, I have continued to
-            pursue my interests in frontend and mobile development with React
-            and React Native.
+            bootcamp in Stockholm, Sweden. 
           </p>
         </div>
       </div>
